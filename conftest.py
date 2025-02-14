ce90d28676c39b64c153366f43c9300f2c4a4980
vitaliy.kuzminskiy <vitalii.kuzminskyi@gmail.com>
2025-02-14 16:36:00 +0100

    tests ready3

--- conftest.py
+++ conftest.py
@@ -6,7 +6,12 @@ from selenium import webdriver
 @pytest.fixture()
 def browser():
     options = Options()
-    options.add_argument('--headless')  #запуск невідімого хрому
+    options.add_argument('--headless')  # Запуск невидимого Chrome
+
+    options.add_argument('--no-sandbox')  # Уникнення конфліктів прав доступу
+    options.add_argument('--disable-dev-shm-usage')  # Уникнення обмежень спільної пам'яті
+    options.add_argument('--disable-gpu')  # Вимикає апаратне прискорення (опціонально)
+
     browser = webdriver.Chrome(options=options)    #відкриває хром
     browser.maximize_window()   #розвертає вікно на увесь экран
     browser.implicitly_wait(3)  #якщо браузер відкриє сторінку і там не буде того що він шукє, він ще 3 секунди буде намагатися це зробити, потім пофейлить
