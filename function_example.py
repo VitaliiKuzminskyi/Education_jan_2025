e294fb04b30b792e6111c744f97ecc9684e8413a
vitaliy.kuzminskiy <vitalii.kuzminskyi@gmail.com>
2025-07-01 21:11:27 +0200

    Hillel lesson 15 - 50%

---
+++ function_example.py
@@ -0,0 +1,14 @@
+a = 5
+b = 6
+
+def function():
+    print(a + b)
+
+def fun_2():
+    print('Alina')
+
+
+function()
+fun_2()
+
+
--- test/inner2/test_setup_teardown_2.py
+++ test/inner2/test_setup_teardown_2.py
@@ -1,5 +1,10 @@
 import pytest
 
+class Dog:
+    color = 'Black'
+    def __init__(self):
+        pass
+
 @pytest.fixture(scope='session')
 def session_fixture():
     print('\nBefore all session')
@@ -7,32 +12,32 @@ def session_fixture():
     print('\nAfter all session')
 
 
-@pytest.fixture(scope='package')
-def package_fixture():
-    print('\nBefore each package')
-    yield
-    print('\nAfter each package')
-
-
-@pytest.fixture(scope='class')
-def class_fixture():
-    print('\nBefore each class')
-    yield
-    print('\nAfter each class')
+# @pytest.fixture(scope='package')
+# def package_fixture():
+#     print('\nBefore each package')
+#     yield
+#     print('\nAfter each package')
+#
+#
+# @pytest.fixture(scope='class')
+# def class_fixture():
+#     print('\nBefore each class')
+#     yield
+#     print('\nAfter each class')
 
 
 @pytest.fixture(scope='function')
-def class_fixture():
+def class_function():
     print('\nBefore each function')
     yield
     print('\nAfter each function')
 
 
-def test_one(session_fixture, package_fixture, class_fixture):
+def test_one(session_fixture, package_fixture, class_fixture, class_function):
     print('\nIs my test 1')
     pass
 
-def test_two(session_fixture, package_fixture, class_fixture):
+def test_two(session_fixture, package_fixture, class_fixture, class_function):
     print('\nIs my test 2')
     pass
 
